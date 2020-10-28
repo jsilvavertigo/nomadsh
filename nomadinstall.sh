886ac8e9d2d9469120ad5626594f29925bc694f2
jsilvavertigo <71832316+jsilvavertigo@users.noreply.github.com>
2020-10-28 10:43:26 -0300

    Update nomadinstall.sh

--- nomadinstall.sh
+++ nomadinstall.sh
@@ -4,5 +4,5 @@ sudo apt-get update && sudo apt-get dist-upgrade -y
 echo @@@@@ Instalando o NOMAD @@@@@ 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install nomad
+sudo apt-get update && sudo apt-get install nomad -y
 echo Instalacao realizada com SUCESSO
